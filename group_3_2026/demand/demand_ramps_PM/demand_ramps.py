6579d2bf96cda6c5aa01d64f6954c80109150a97
Pelayo <pelayinco@gmail.com>
2026-05-25 14:20:34 +0200

    Loading the cleaned data without germany

--- group_3_2026/demand/demand_ramps_PM/demand_ramps.py
+++ group_3_2026/demand/demand_ramps_PM/demand_ramps.py
@@ -1,19 +1,39 @@
-import os
 import pandas as pd
 
-# loading the cleaned data for France, Germany, and Spain
-df_fr = pd.read_csv("data\\processed\\france_cleaned.csv")
-df_de = pd.read_csv("data\\processed\\germany_cleaned.csv")
-df_es = pd.read_csv("data\\processed\\spain_cleaned.csv")
-# Ensure data is sorted by time
-df_fr = df_fr.sort_values(by='timestamp').reset_index(drop=True)
-df_de = df_de.sort_values(by='timestamp').reset_index(drop=True)
-df_es = df_es.sort_values(by='timestamp').reset_index(drop=True)
-# Calculate the hourly ramp
-df_fr['rampa'] = df_fr['demand'].diff()
-df_de['rampa'] = df_de['demand'].diff()
-df_es['rampa'] = df_es['demand'].diff()
+france = pd.read_csv(
+    "group_3_2026/data/raw/France_demand_year_2024.csv",
+    sep="\t",
+    encoding="latin1"
+)
 
-print("¡Rampas calculadas con éxito!")
+spain_1 = pd.read_csv(
+    "group_3_2026/data/raw/Spain-2024-05-10-Seguimiento de la demanda de energía eléctrica (MW).csv",
+    skiprows=2,
+    encoding="latin1",
+    header=None,
+    usecols=[0, 1]
+)
 
+spain_2 = pd.read_csv(
+    "group_3_2026/data/raw/Spain-2024-05-11-Seguimiento de la demanda de energía eléctrica (MW).csv",
+    skiprows=2,
+    encoding="latin1",
+    header=None,
+    usecols=[0, 1]
+)
 
+spain_3 = pd.read_csv(
+    "group_3_2026/data/raw/Spain-2024-05-12-Seguimiento de la demanda de energía eléctrica (MW).csv",
+    skiprows=2,
+    encoding="latin1",
+    header=None,
+    usecols=[0, 1]
+)
+
+spain_4 = pd.read_csv(
+    "group_3_2026/data/raw/Spain-2024-05-13-Seguimiento de la demanda de energía eléctrica (MW).csv",
+    skiprows=2,
+    encoding="latin1",
+    header=None,
+    usecols=[0, 1]
+)
